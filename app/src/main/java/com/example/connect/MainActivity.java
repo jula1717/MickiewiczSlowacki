dd3036c345257d8076112eb713aaf66e86e36ad8
jula1717 <julaplocienniczak@gmail.com>
2022-06-09 22:35:26 +0200

    after the move the player change, make it impossible to click on a filled field

--- app/src/main/java/com/example/connect/MainActivity.java
+++ app/src/main/java/com/example/connect/MainActivity.java
@@ -13,12 +13,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    int [] board = {2,2,2,2,2,2,2,2,2}; //2 means empty, 1 means Słowacki, 0 means Mickiewicz
+    boolean ActivePlayer = false; // false Mickiewicz | true Słowacki
     public void drop(View view) {
         ImageView clicked = (ImageView) view;
-        clicked.setTranslationY(-200f);
-        clicked.setImageResource(R.drawable.mickiewicz);
-        clicked.animate().translationYBy(200f).setDuration(300);
+        int clickedOnBoard = Integer.parseInt(clicked.getTag().toString());
+        if (board[clickedOnBoard]==2)
+        {
+            clicked.setTranslationY(-200f);
+            if(ActivePlayer==false)
+            {
+                board[clickedOnBoard]=0;
+                clicked.setImageResource(R.drawable.mickiewicz);
+                ActivePlayer=true;
+            }
+            else
+            {
+                board[clickedOnBoard]=1;
+                clicked.setImageResource(R.drawable.slowacki);
+                ActivePlayer=false;
+            }
+            clicked.animate().translationYBy(200f).setDuration(300);
+        }
         System.out.println("clicked");
     }
 }
