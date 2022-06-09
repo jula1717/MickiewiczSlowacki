06015061ec2475ebc33e00cf587ff446a7afd408
jula1717 <julaplocienniczak@gmail.com>
2022-06-09 20:37:15 +0200

    drop animantion

--- app/src/main/java/com/example/connect/MainActivity.java
+++ app/src/main/java/com/example/connect/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.connect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void drop(View view) {
+        ImageView clicked = (ImageView) view;
+        clicked.setTranslationY(-200f);
+        clicked.setImageResource(R.drawable.mickiewicz);
+        clicked.animate().translationYBy(200f).setDuration(300);
+        System.out.println("clicked");
     }
 }
