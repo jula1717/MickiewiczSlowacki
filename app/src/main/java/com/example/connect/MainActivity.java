571d89b9979bca30efa88f38571971adb6f4b36d
jula1717 <julaplocienniczak@gmail.com>
2022-06-10 15:04:25 +0200

    Reset board on click PlayAgainButton

--- app/src/main/java/com/example/connect/MainActivity.java
+++ app/src/main/java/com/example/connect/MainActivity.java
@@ -4,39 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import androidx.gridlayout.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
     TextView txtWinnerIs;
+    LinearLayout linearLayout;
+    //GridLayout gridLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtWinnerIs = (TextView) findViewById(R.id.txtWinnerIs);
+        linearLayout = (LinearLayout) findViewById(R.id.GameOverLayout);
+    //    gridLayout = (GridLayout) findViewById(R.id.gridLayout);
     }
     int [] board = {2,2,2,2,2,2,2,2,2}; //2 means empty, 1 means Słowacki, 0 means Mickiewicz
     int [][] winningPositions = {{0,3,6},{1,4,7},{2,5,8},{0,1,2},{3,4,5},{6,7,8},{0,4,8},{2,4,6}};
-    boolean ActivePlayer = false; // false Mickiewicz | true Słowacki
+    boolean activePlayer = false; // false Mickiewicz | true Słowacki
     public void drop(View view) {
         ImageView clicked = (ImageView) view;
         int clickedOnBoard = Integer.parseInt(clicked.getTag().toString());
         if (board[clickedOnBoard]==2)
         {
             clicked.setTranslationY(-200f);
-            if(ActivePlayer==false)
+            if(activePlayer ==false)
             {
                 board[clickedOnBoard]=0;
                 clicked.setImageResource(R.drawable.mickiewicz);
-                ActivePlayer=true;
+                activePlayer =true;
                 System.out.println("Board["+clickedOnBoard+"]="+board[clickedOnBoard]);//
             }
             else
             {
                 board[clickedOnBoard]=1;
                 clicked.setImageResource(R.drawable.slowacki);
-                ActivePlayer=false;
+                activePlayer =false;
                 System.out.println("Board["+clickedOnBoard+"]="+board[clickedOnBoard]);
             }
             clicked.animate().translationYBy(200f).setDuration(300);
@@ -48,16 +55,24 @@ public class MainActivity extends AppCompatActivity {
                 {
                     String winner = winningPosition[0]==0?"Mickiewicz":"Słowacki";
                     txtWinnerIs.setText("The winner is "+winner);
-                    LinearLayout linearLayout = (LinearLayout) findViewById(R.id.GameOverLayout);
                     linearLayout.setVisibility(View.VISIBLE);
                 }
             }
         }
-        System.out.println("clicked");
     }
     public void playAgain(View view)
     {
-        //int clickedOnBoard = Integer.parseInt(clicked.getTag().toString());
-        //clicked.setImageResource(R.drawable.mickiewicz);
+        linearLayout.setVisibility(View.INVISIBLE);
+        for (int field:board
+        ) {
+            field=2;
+        }
+        activePlayer = activePlayer==true?false:true;
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+        for(int i=0;i<gridLayout.getChildCount();i++)
+        {
+            ((ImageView)gridLayout.getChildAt(i)).setImageResource(0);
+        }
+
     }
 }
