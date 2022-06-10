cac85954a55d3c28c9f90830862fe0fc7e712690
jula1717 <julaplocienniczak@gmail.com>
2022-06-10 14:36:39 +0200

    GameOverLayout create, the winner display

--- app/src/main/java/com/example/connect/MainActivity.java
+++ app/src/main/java/com/example/connect/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txtWinnerIs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtWinnerIs = (TextView) findViewById(R.id.txtWinnerIs);
     }
     int [] board = {2,2,2,2,2,2,2,2,2}; //2 means empty, 1 means Słowacki, 0 means Mickiewicz
     int [][] winningPositions = {{0,3,6},{1,4,7},{2,5,8},{0,1,2},{3,4,5},{6,7,8},{0,4,8},{2,4,6}};
@@ -43,11 +46,18 @@ public class MainActivity extends AppCompatActivity {
                         board[winningPosition[1]]==board[winningPosition[2]]&&      //jeśli na wszystkich trzech polach zwycięskiej kombinacji jest ten sam stan
                         board[winningPosition[0]]!=2)                               //ale nie jest to puste pole
                 {
-                    System.out.println("The winner is "+board[winningPosition[0]] );
+                    String winner = winningPosition[0]==0?"Mickiewicz":"Słowacki";
+                    txtWinnerIs.setText("The winner is "+winner);
+                    LinearLayout linearLayout = (LinearLayout) findViewById(R.id.GameOverLayout);
+                    linearLayout.setVisibility(View.VISIBLE);
                 }
             }
         }
         System.out.println("clicked");
-        //test GitHub
+    }
+    public void playAgain(View view)
+    {
+        //int clickedOnBoard = Integer.parseInt(clicked.getTag().toString());
+        //clicked.setImageResource(R.drawable.mickiewicz);
     }
 }
